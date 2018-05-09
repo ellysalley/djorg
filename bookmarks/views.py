d3e0f7e573464580de51aca80486d0c87239e752
Elly <ellyhahn@gmail.com>
2018-05-09 16:09:18 -0700

    bookmark app partially working

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .forms import BookmarkForm
+from .models import Bookmark
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        form = BookmarkForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = { 'bookmarks': Bookmark.objects.all(), 
+                'form': BookmarkForm(), 
+                'action': 'Create', 
+                'button': 'Create',
+                }
+    return render(request, 'bookmarks/index.html', context)
+
+def update(request, bookmark_id):
+    bookmark = Bookmark.object.get(pk=bookmark_id)
+    if request.method == 'POST':
+        form = BookmarkForm(request.POST, instance=bookmark)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/bookmarks')
+    context = { 'bookmarks': Bookmark.objects.all(),
+                'form': BookmarkForm(),
+                'action': 'Update',
+                'button': 'Update',
+                }
+    return render(request, 'bookmarks/index.html', context)
+
+def delete(request, bookmark_id):
+    bookmark = Bookmark.objects.get(pk=bookmark_id)
+    if bookmark:
+        bookmark.delete()
+        return HttpResponseRedirect('/bookmarks')
+            
